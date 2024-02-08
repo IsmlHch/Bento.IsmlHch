34406bbc651b39e09bf76db931c3d76775f125e6
IsmlHch <159464215+IsmlHch@users.noreply.github.com>
2024-02-08 17:53:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: 'c4c125146c9d998f037dc7c611af7e77', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
